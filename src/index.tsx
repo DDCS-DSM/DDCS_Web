be15cdeb95482a4c4168cef3a3ff85885ff2e0a5
biscrab <biscrab@naver.com>
2022-04-12 23:28:20 +0900

    📑 :: 리듀서 설치

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import GlobalStyle from "./styles/App";
+import { Provider } from "react-redux";
+import store from './store/store'
 
 ReactDOM.render(
+  <Provider store={store}>
   <Router>
     <GlobalStyle />
     <App />
-  </Router>,
+  </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
