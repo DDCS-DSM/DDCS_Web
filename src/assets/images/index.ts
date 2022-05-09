29ac832f49b995716c050ae65cdf4f19e2239147
biscrab <biscrab@naver.com>
2022-05-09 19:44:48 +0900

    ✏️ :: 배너 이미지 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,7 @@
-export { default as banner } from "./banner.png";
+export { default as home } from "./home.png";
+export { default as list } from "./list.png"
+export { default as enlist } from "./enlist.png";
+export { default as accept } from "./accept.png";
 export { default as bike } from "./bike.png";
 export { default as box } from "./box.png";
 export { default as logo } from "./logo.png";
