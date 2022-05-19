499f457056ef1a0297625f6773c7ac86de85cc77
__Rals <winhave@naver.com>
2022-05-19 16:04:34 +0900

    ✏️ :: App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import { useState } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
-import { Home, Privacy, Enlist, List, Accept } from './pages';
+import { Home, Privacy, Enlist, List, Accept } from "./pages";
 
 import GlobalStyle from "./styles";
 import Title from "./components/Title";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import LoginModal from "./components/LoginModal";
+import PolicyModal from "./components/PolicyModal";
 import RegisterModal from "./components/RegisterModal";
 
 axios.defaults.baseURL = "";
 
 function App() {
-  
   const [loginState, setLoginState] = useState<boolean>(false);
   const [loginVisible, setLoginVisible] = useState<boolean>(false);
+  const [policyVisible, setPolicyVisible] = useState<boolean>(false);
   const [registerVisible, setRegisterVisible] = useState<boolean>(false);
 
   return (
@@ -26,12 +27,20 @@ function App() {
         <LoginModal
           setLoginVisible={setLoginVisible}
           setLoginState={setLoginState}
+          setPolicyVisible={setPolicyVisible}
+        />
+      )}
+      {policyVisible && (
+        <PolicyModal
+          setLoginVisible={setLoginVisible}
+          setPolicyVisible={setPolicyVisible}
           setRegisterVisible={setRegisterVisible}
         />
       )}
       {registerVisible && (
         <RegisterModal
           setLoginVisible={setLoginVisible}
+          setPolicyVisible={setPolicyVisible}
           setRegisterVisible={setRegisterVisible}
         />
       )}
@@ -44,7 +53,7 @@ function App() {
         <Route path="/enlist" element={<Enlist />} />
         <Route path="/accept" element={<Accept />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
