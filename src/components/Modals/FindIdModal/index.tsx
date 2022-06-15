66d827cdc10d473c99c6b7c644fa16fc09320d2f
__Rals <winhave@naver.com>
2022-06-15 17:29:32 +0900

    ✏️ :: 아이디 찾기 모달 추가

---
+++ src/components/Modals/FindIdModal/index.tsx
@@ -0,0 +1,26 @@
+import { useRef } from "react";
+import * as S from "./styles";
+
+interface LoginModalInterface {
+  setModalState: React.Dispatch<React.SetStateAction<String>>;
+}
+
+const FindIdModal = ({ setModalState }: LoginModalInterface) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  return (
+    <S.Filter>
+      <S.Background>
+        <S.Close onClick={() => setModalState("login")}>←</S.Close>
+        <S.Title>아이디 찾기</S.Title>
+        <S.Wrapper>
+          <S.Input ref={inputRef} placeholder="이름" />
+          <S.Input type="password" placeholder="이메일" />
+          <S.Button>아이디 찾기</S.Button>
+        </S.Wrapper>
+      </S.Background>
+    </S.Filter>
+  );
+};
+
+export default FindIdModal;
