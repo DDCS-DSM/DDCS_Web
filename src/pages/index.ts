219477a43cbbfe230154e70f96016912691a4883
biscrab <biscrab@naver.com>
2022-07-08 11:35:29 +0900

    📝 ::  Developers 페이지 추가 누락 파일 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export { default as Enlist } from './Enlist';
 export { default as Home } from './Home';
 export { default as List } from './List';
 export { default as Privacy } from './Privacy';
+export { default as Developers } from './Developers';
